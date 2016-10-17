43e3813d8b3cbe268a2fc75efd862b87488454a3
Chris Underwood <cunderwoodmn@gmail.com>
2016-10-16 17:10:28 -0700

    add code to check that we are grabbing the href

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
 //Problem: User when clicking on image goes to a dead end
 //Solution: Create an overlay with the large image - Lightbox
+
+// 1. Capture the click event on a link to an image
+$('#imageGallery a').click(function(event) {
+  event.preventDefault();
+  var melanie = $(this).attr("href");
+  console.log(melanie);
+  // 1.1 Show the overlay
+  // 1.2 Update the overlay with the image linked in the link
+  // 1.3 Get child alt attribute and set caption
+});
+
+// 2. Add on overlay
+  // 2.1 And an image
+  // 2.2 Add a caption
+// 3. When overlay is clicked hide the overlay
